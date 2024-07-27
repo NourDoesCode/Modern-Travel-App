0f9a9123f41bd016cc7a5933ede0e4f61883bfd6
Nour <nourpow@gmail.com>
2024-07-27 19:20:29 +0100

    Styling navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,40 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+import Button from "./Button";
 
 function Navbar() {
   return (
     <nav className="border-4 border-red-500 flex items-center justify-between px-6 lg:px-20 3xl:px-0 relative z-30 py-5">
       <Link href="/">
-        <img src="" alt="" />
+        <Image
+          className="pl-5"
+          src="/hilink-logo.svg"
+          alt="logo"
+          width={90}
+          height={40}
+        ></Image>
       </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            className="border-4 border-green-500 regular-16 text-gray-50 flex items-center justify-center  pb-1.5 transition-all hover:font-bold"
+            href={link.href}
+            key={link.key}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="hidden lg:flex lg:items-center lg:justify-center">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
     </nav>
   );
 }
